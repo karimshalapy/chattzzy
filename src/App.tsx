17fcab594006c6aeb3128929d3fb043c0342b9c1
Karim Shalapy <karimshalapy@gmail.com>
2021-01-24 23:33:47 +0200

    feat: render app header with signout btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import classes from './App.module.scss';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './Firebase';
+import Button from './components/Button/Button';
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <div className={classes.App}>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <div>
+          {
+            user
+              ? <Button variant="secondary" onClick={() => { auth.signOut() }}>Sign Out</Button>
+              : null
+          }
+        </div>
+      </header>
     </div>
   );
 }
