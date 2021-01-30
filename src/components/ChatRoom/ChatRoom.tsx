bd9a7d53988a9cee3da23ad865c259c95c9cf2b5
Karim Shalapy <karimshalapy@gmail.com>
2021-01-30 19:34:05 +0200

    style: remove excess console logs

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, memo } from 'react'
 import { firestore } from '../../Firebase'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import ChatMessage from './ChatMessage/ChatMessage'
-// import classes from
 
 interface Props {
 
@@ -16,7 +15,6 @@ const ChatRoom: React.FC<Props> = props => {
     const messageElementRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        console.log(messages)
         messageElementRef.current?.scrollIntoView({ behavior: "smooth" })
     }, [messages])
 
